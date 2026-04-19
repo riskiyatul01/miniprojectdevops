a09237de887b1ec68875cca80a2f5f376492d25a
KenasGaleno <150374973+KenasGaleno@users.noreply.github.com>
2026-04-19 16:45:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Kelompok 3!');
+    res.json({
+        message: "Hello from DevSecOps Pipeline!",
+        status: "Running",
+        environment: process.env.NODE_ENV || "development"
+    });
 });
 
+// Endpoint untuk Healthcheck (Standard High Availability)
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok' });
+    res.status(200).send('OK');
 });
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
+    console.log(`App is running on port ${PORT}`);
 });
