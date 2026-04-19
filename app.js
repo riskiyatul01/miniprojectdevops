acf023e47b62de5e4661d9c5af0f0937b9528433
bidding rebola <laku6@MacBook-Pro-M3.local>
2026-04-19 11:04:22 +0700

    update teks aplikasi untuk testing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker App!');
+  res.send('Hello from Kelompok 3!');
 });
 
 app.get('/health', (req, res) => {
